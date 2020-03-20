825d98eff468ba0347ea5dee27afebfec18b9813
add-plus <add-plus@protonmail.com>
2020-03-20 17:10:20 +0800

    update

---
+++ docs/precache-manifest.9037e37fdb7cb220871aa3a9f7e348f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a624057ba4846495222dcc8626b79f71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "235d543de9ef5cde1db9",
+    "url": "./static/css/2.145688f1.chunk.css"
+  },
+  {
+    "revision": "c8885a7bbd6362488b19",
+    "url": "./static/css/main.239626be.chunk.css"
+  },
+  {
+    "revision": "235d543de9ef5cde1db9",
+    "url": "./static/js/2.ac8fd8dd.chunk.js"
+  },
+  {
+    "revision": "c8885a7bbd6362488b19",
+    "url": "./static/js/main.f4dcd360.chunk.js"
+  },
+  {
+    "revision": "39119ff7bb8f60721d03",
+    "url": "./static/js/runtime~main.d669785e.js"
+  },
+  {
+    "revision": "882488760283110e7bd2438cd3587ef9",
+    "url": "./static/media/logo.88248876.png"
+  }
+]);
---
+++ src/component/contract.js
@@ -0,0 +1,17 @@
+class Contract{
+
+    address = "4MVFmoC4po6uuCM44cvBgmKMJyuHbZiQBxZTrjXLdsqVLUA5RyfGTbWRBvYj4Er8ZBvZcW9ksy3zBgkpf4mfQgEt";
+
+    abi = [{"constant":false,"inputs":[{"name":"refereeCode","type":"string"}],"name":"invest","outputs":[{"name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"detailsOfIncome","outputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"triggerStaticProfit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"setMarketAddr","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"details","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint8"},{"name":"","type":"bool"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"withdrawBalance","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"registerNode","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"calcuStaticProfit","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"id","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"balanceOfSero","outputs":[{"name":"amount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"code","type":"string"}],"name":"codeExist","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_triggerStaticNum","type":"uint256"}],"name":"setTriggerStaticNum","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_marketAddr","type":"address"},{"name":"_codeServiceAddr","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}]
+
+    dapp = {
+        name: "add",
+        contractAddress: "4MVFmoC4po6uuCM44cvBgmKMJyuHbZiQBxZTrjXLdsqVLUA5RyfGTbWRBvYj4Er8ZBvZcW9ksy3zBgkpf4mfQgEt",
+        github: "https://github.com/add-plus/",
+        author: "add-plus",
+        url: document.location.href,
+        logo: document.location.protocol + '//' + document.location.host + '/add/logo.png'
+    };
+}
+
+export default Contract
